669d8862ca20680acea2615c91018564c6174052
menuchabern <menucha9021@gmail.com>
2024-11-27 17:20:53 -0500

    added sample data

--- 1 table.sql
+++ 1 table.sql
@@ -1,3 +1,6 @@
 use HumanBodyDB
 GO
-create table dbo.body(ID not null identity primary key)
+create table dbo.body(ID not null identity primary key
+    bodypart varchar(100) not null, 
+    num int not null
+    )
