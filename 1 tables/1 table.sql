055f6c444595b6d9d922d8a011069eb1b61ce4f7
menuchabern <menucha9021@gmail.com>
2024-12-02 20:44:55 -0500

    added planet to body table

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -3,5 +3,6 @@ GO
 drop table if exists body
 create table dbo.body(ID int not null identity primary key,
     bodypart varchar(100) not null, 
+    planet varchar (50) not null,
     num int null
     )
