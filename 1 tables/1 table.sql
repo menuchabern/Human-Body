f1716618c0f66145af5e4756cf5a392d29f280a7
menuchabern <menucha9021@gmail.com>
2024-12-02 20:36:56 -0500

    1st commit for the trial branch

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,6 +1,9 @@
 use HumanBodyDB
+go
+drop table if exists body
 GO
 create table dbo.body(ID int not null identity primary key,
+	creaturetype varchar(25) not null,
     bodypart varchar(100) not null, 
     num int not null
     )
