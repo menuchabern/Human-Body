2f20a356dbb80faf0d31b36a719578b8713f3729
menuchabern <menucha9021@gmail.com>
2024-12-02 20:30:09 -0500

    made the num colummn nullable

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,6 +1,7 @@
 use HumanBodyDB
 GO
+drop table if exists body
 create table dbo.body(ID int not null identity primary key,
     bodypart varchar(100) not null, 
-    num int not null
+    num int null
     )
